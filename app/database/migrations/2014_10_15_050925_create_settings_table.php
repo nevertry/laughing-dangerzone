385d4a4a3414407d71b64e24d3e0d4f8990c8633
Hendy <hendy.yanuar@kiranatama.com>
2014-10-15 14:08:14 +0700

    Migration: create settings table

---
+++ app/database/migrations/2014_10_15_050925_create_settings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('settings', function($table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->longText('meta_data');
+
+			$table->timestamps();
+			$table->unique('name');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('settings');
+	}
+
+}
