6df6c1be4b27e38377acaeab5930869406482e06
Hendy <hendy.yanuar@kiranatama.com>
2014-10-15 11:23:01 +0700

    Migrataion: User Access

---
+++ app/database/migrations/2014_10_15_034417_create_users_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('email');
+			$table->string('password');
+			$table->longText('permissions')->nullable();
+			$table->boolean('activated')->default(0);
+			$table->string('activation_code')->nullable();
+			$table->timestamp('activated_at')->nullable();
+			$table->timestamp('last_login')->nullable();
+			$table->string('persist_code')->nullable();
+			$table->string('reset_password_code')->nullable();
+			$table->string('first_name')->nullable();
+			$table->string('last_name')->nullable();
+			$table->timestamps();
+			$table->softDeletes();
+
+			$table->unique('email');
+			$table->index('activation_code');
+			$table->index('reset_password_code');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
---
+++ app/database/migrations/2014_10_15_040453_create_users_groups_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersGroupsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users_groups', function($table)
+		{
+			$table->integer('user_id')->unsigned();
+			$table->integer('group_id')->unsigned();
+
+			$table->primary(array('user_id', 'group_id'));
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users_groups');
+	}
+
+}
