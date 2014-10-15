968a9f773f70b189b5aae4beb35936e89be9de0c
Hendy <hendy.yanuar@kiranatama.com>
2014-10-16 00:22:23 +0700

    Locale: Add text caption

---
+++ app/lang/en/captions.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+	'user.sign_in' => 'Sign In',
+	'user.user_id' => 'User ID',
+	'user.password' => 'Password',
+	'user.remember' => 'Remember me',
+	'user.sign_in_btn' => 'Sign me in',
+	'user.forgot_link' => 'I forgot my password',
+	'user.register_link' => 'Register a new membership',
+);
---
+++ app/lang/id/captions.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'user.sign_in' => 'Masuk',
+	'user.user_id' => 'Email',
+	'user.password' => 'Sandi',
+	'user.remember' => 'Tetap masuk',
+	'user.sign_in_btn' => 'Masuk',
+	'user.forgot_link' => 'Saya tidak tahu sandi saya',
+	'user.register_link' => 'Buat akun',
+	'user.sign_out' => 'Sukses keluar.',
+);
