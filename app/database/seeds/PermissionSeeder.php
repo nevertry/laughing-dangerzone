0ac116c8078edc96bb28d6ed54553f9a3b060a5e
Hendy <hendy.yanuar@kiranatama.com>
2014-10-21 20:38:11 +0700

    Permissions set

--- app/database/seeds/PermissionSeeder.php
+++ app/database/seeds/PermissionSeeder.php
@@ -11,43 +11,84 @@ class PermissionSeeder extends Seeder {
 	{
 		DB::table('permissions')->truncate();
 
-		$permissions_groups = array(
-			// Dashboard
-			'dashboard' => array(
-				'dashboard'
-				),
+/*
+masterdata	
+masterdata.unit	masterdata.unit.crud
+masterdata.pekerjaan	masterdata.pekerjaan.crud
+masterdata.peralatan	masterdata.peralatan.crud
+*/
+
+		$permissions_groups = [
+			// Master Data
+			'masterdata' => [
+				'masterdata.unit',
+				'masterdata.pekerjaan',
+				'masterdata.peralatan',
+				// CRUD
+				'masterdata.unit.crud',
+				'masterdata.pekerjaan.crud',
+				'masterdata.peralatan.crud'
+				],
+
+			// Tarif
+			'tarif' => [
+				'tarif.sk',
+				'tarif.jenis',
+				'tarif.peruntukan',
+				// CRUD
+				'tarif.sk.crud',
+				'tarif.jenis.crud',
+				'tarif.peruntukan.crud'
+				],
+
+			// Akuntansi
+			'akuntansi' => [
+				// CoA
+				'akuntansi.akun',
+				'akuntansi.akun.crud',
+				// Jurnal
+				'akuntansi.jurnal',
+				'akuntansi.jurnal.crud'
+			],
+
+			// Pelanggan
+			'pelanggan' => [
+				'pelanggan.lihat',
+				'pelanggan.lihat.semua',
+				'pelanggan.lihat.calon',
+				'pelanggan.lihat.teguran',
+				'pelanggan.lihat.nonaktif',
+				'pelanggan.lihat.dicabut',
+				// CRUD
+				'pelanggan.crud',
+				// Atur Tagihan / Kasir
+				'pelanggan.tagihan'
+			],
+
+			// Tagihan
+			'tagihan' => [
+				'tagihan.bulanan',
+				'tagihan.rab',
+				// CRUD
+				'tagihan.bulanan.crud',
+				'tagihan.rab.crud'
+			],
+
+			// Catat Meter
+			'catatmeter' => [
+				'catatmeter.tambah'
+			],
 
 			// Pengguna
-			'pengguna' => array(
-				'user',
-				),
-
-			// Groups
-			'group' => array(
-				'group',
-				),
-
-			// Menus
-			'menu' => array(
-				// Menus : tidak perlu
-				//'menu',
-
-				// Master Data
-				'menu.Masterdata',
-
-				// Akuntansi
-				'menu.Akuntansi',
-
-				// Pengguna
-				'menu.Pengguna',
-				'menu.SubPenggunaProfile',
-				'menu.SubPenggunaList',
-				'menu.SubPenggunaGroup',
-
-				// Pengaturan
-				'menu.Pengaturan',
-				)
-			);
+			'pengguna' => [
+				'pengguna.profil',
+				'pengguna.id',
+				'pengguna.grup',
+				// CRUD
+				'pengguna.id.crud',
+				'pengguna.grup.crud',
+			]
+			];
 
 		foreach ($permissions_groups as $permissions)
 		{
