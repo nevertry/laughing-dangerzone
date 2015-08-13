1db9ee5ad26da57c3e493b5f6ade6816cf51d070
hendyyanuar <hendyyanuar@gmail.com>
2015-08-13 11:33:17 +0700

    Set application environment using .env.php file

---
+++ .env.DIST.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * DIST SETTINGS
+ *
+ * Copy, rename to .env.ENVIRONMENT.php, and configure accordingly. For
+ * local sandboxes, rename to .env.local.php. For production, rename to
+ * .env.production.php.
+ *
+ * See http://laravel.com/docs/4.2/configuration#protecting-sensitive-configuration
+ * for details.
+ */
+return array(
+	'DATABASE_HOST'     => '',
+	'DATABASE_NAME'     => '',
+	'DATABASE_USER'     => '',
+	'DATABASE_PASSWORD' => ''
+);
