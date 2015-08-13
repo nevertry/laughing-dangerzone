f5d95dcf45ea63b7f432dafa0847b266ab162236
hendyyanuar <hendyyanuar@gmail.com>
2015-08-13 11:43:24 +0700

    Set default menu to en, Add menu translation

---
+++ app/lang/en/menu.php
@@ -0,0 +1,34 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Menu Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used by the menu appears on dashboard.
+	|
+	*/
+
+	/*
+	|--------------------------------------------------------------------------
+	| Backend
+	|--------------------------------------------------------------------------
+	*/
+
+	# Analytics
+	'analytics' => 'Analytics',
+	'analytics.answered_riddles' => 'Answered Riddles',
+	'analytics.riddle_winners' => 'Riddle Winners',
+
+	# Riddles
+	'riddles' => 'Riddles',
+	'riddles.list' => 'Riddles List',
+	'riddles.create' => 'Create new Riddles',
+
+	# Riddles
+	'guests' => 'Guest',
+	'guests.list' => 'Guests List',
+
+);
