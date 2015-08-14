ef5b8eb6d7c7be7613b564c13ed6dde7474424f1
hendyyanuar <hendyyanuar@gmail.com>
2015-08-14 18:11:24 +0700

    Riddle creator html adjustment.

--- public/themes/admin/views/pages/riddles/create.blade.php
+++ public/themes/admin/views/pages/riddles/create.blade.php
@@ -22,15 +22,14 @@
 
                                 <!-- form start -->
                                 {{ Form::open(array('action' => 'dashboard.riddles.create', 'role' => 'form')) }}
-                                {{ Form::token() }}
                                     <div class="box-body">
 
                                         <div class="row">
-                                            <div class="form-group col-md-3 {{ setClassHasError($errors->has('type')) }}">
+                                            <div class="form-group col-sm-3 {{ setClassHasError($errors->has('type')) }}">
                                                 {{ Form::label('riddle_type', "Type") }}
                                                 {{ Form::select('riddle_type', Riddle::getContentTypes(), ifset($riddle_data['riddle_type']), ['class'=>"form-control"]) }}
                                             </div>
-                                            <div class="form-group col-md-3 col-md-offset-3 {{ setClassHasError($errors->has('publish_status')) }}">
+                                            <div class="form-group col-sm-3 col-sm-offset-3 {{ setClassHasError($errors->has('publish_status')) }}">
                                                 {{ Form::label('riddle_publish_status', "Pubish Status") }}
                                                 {{ Form::select('riddle_publish_status', Riddle::getPublishStatuses(), ifset($riddle_data['riddle_publish_status'], 1), ['class'=>"form-control"]) }}
                                             </div>
@@ -41,7 +40,7 @@
                                                 {{ Form::label('riddle_content', "Content") }}
                                                 {{ Form::textarea('riddle_content', ifset($riddle_data['riddle_content']), ['class'=>"form-control", 'placeholder'=>'(ex.: Can be text, or URL to image/video/audio)', 'rows'=>'3']) }}
                                             </div>
-                                            <div class="form-group col-md-6 col-md-offset-0 {{ setClassHasError($errors->has('question')) }}">
+                                            <div class="form-group col-sm-6 col-sm-offset-0 {{ setClassHasError($errors->has('question')) }}">
                                                 {{ Form::label('riddle_question', "Question") }}
                                                 {{ Form::textarea('riddle_question', ifset($riddle_data['riddle_question']), ['class'=>"form-control", 'placeholder'=>'(ex.: What is the name of the biggest lake in Indonesia? ...)', 'rows'=>'3']) }}
                                             </div>
@@ -63,8 +62,6 @@
                                     <div class="box-footer">
                                         {{ Form::button("Save &amp; Continue", ['class'=>"btn btn-primary", 'type'=>"submit", 'name'=>"createMore", 'value'=>"1"]) }}
                                         {{ Form::button("Save", ['class'=>"btn btn-primary", 'type'=>"submit", 'name'=>"createOnce", 'value'=>"1"]) }}
-                                        <!-- <button type="submit" class="btn btn-primary" name="">Save &amp; Continue</button> -->
-                                        <!-- <button type="submit" class="btn btn-primary" name="submit">Submit</button> -->
                                     </div>
                                 {{ Form::close() }}
                             </div><!-- /.box -->
