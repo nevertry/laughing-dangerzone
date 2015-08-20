aa2ac17aa3f841b5c8d0fb998e2535e4b1f64c70
hendyyanuar <hendyyanuar@gmail.com>
2015-08-20 18:52:10 +0700

    Merge down XApi into API Base Controller

--- app/controllers/api/v1/BaseApiController.php
+++ app/controllers/api/v1/BaseApiController.php
@@ -13,10 +13,34 @@ class BaseApiController extends BaseController
 
 	public static function response()
 	{
-		return \XApi::response([
+		return self::generator([
 			'error' => self::$error,
 			'message' => self::$message,
 			'data' => self::$data,
 		], self::$code);
 	}
+
+    public static function generator($data = array('error' => 0, 'data' => null, 'message' => null), $http_code = 200)
+    {
+        return \Response::json(
+            array(
+                'error' =>   $data['error'],
+                'message' => empty($data['message']) ? null : $data['message'],
+                'data' => empty($data['data']) ? null : $data['data']
+            ),
+            $http_code
+        );
+    }
+
+    public static function parser($datas, $error = 0, $numeric_check = true)
+    {
+        $results = array();
+        $results['count'] = count($datas);
+        $results['data'] = ($numeric_check) ? json_decode(json_encode($datas, JSON_NUMERIC_CHECK)) : $datas;
+
+        return self::generator(array(
+                'data' => $results,
+                'error' => $error
+            ));
+    }
 }
