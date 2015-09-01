a754d00dee771952c6c8ba44cfc0f84b14f7193f
Hendy Yanuar <hendy.yanuar@kiranatama.com>
2015-09-01 18:34:42 +0700

    Indentation adjustment

--- app/controllers/api/v1/BaseApiController.php
+++ app/controllers/api/v1/BaseApiController.php
@@ -20,27 +20,27 @@ class BaseApiController extends BaseController
 		], self::$code);
 	}
 
-    public static function generator($data = array('error' => 0, 'data' => null, 'message' => null), $http_code = 200)
-    {
-        return \Response::json(
-            array(
-                'error' =>   $data['error'],
-                'message' => empty($data['message']) ? null : $data['message'],
-                'data' => empty($data['data']) ? null : $data['data']
-            ),
-            $http_code
-        );
-    }
+	public static function generator($data = array('error' => 0, 'data' => null, 'message' => null), $http_code = 200)
+	{
+		return \Response::json(
+			array(
+				'error' =>   $data['error'],
+				'message' => empty($data['message']) ? null : $data['message'],
+				'data' => empty($data['data']) ? null : $data['data']
+			),
+			$http_code
+		);
+	}
 
-    public static function parser($datas, $error = 0, $numeric_check = true)
-    {
-        $results = array();
-        $results['count'] = count($datas);
-        $results['data'] = ($numeric_check) ? json_decode(json_encode($datas, JSON_NUMERIC_CHECK)) : $datas;
+	public static function parser($datas, $error = 0, $numeric_check = true)
+	{
+		$results = array();
+		$results['count'] = count($datas);
+		$results['data'] = ($numeric_check) ? json_decode(json_encode($datas, JSON_NUMERIC_CHECK)) : $datas;
 
-        return self::generator(array(
-                'data' => $results,
-                'error' => $error
-            ));
-    }
+		return self::generator(array(
+				'data' => $results,
+				'error' => $error
+			));
+	}
 }
